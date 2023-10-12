29a800e20547dcf59a584a4ebe29e77cc4fb155f
fassty <mikulaspa@gmail.com>
2023-10-12 21:04:45 +0200

    Add CLI options for workers, envs, episode length, num samples and run_name

--- rl_trading/run_experiment.py
+++ rl_trading/run_experiment.py
@@ -15,26 +15,33 @@ from rl_trading.simulation.env import *
 
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--exp_name", default='R2D2_50M', type=str, help="Experiment name.")
-parser.add_argument("--save_dir", default='/home/fassty/Devel/school/diploma_thesis/code/exp_results/long_exp/R2D2', type=str, help="Where to store the experiments.")
+parser.add_argument("--exp_group", default='normalized_reward', type=str, help="Experiment group.")
+parser.add_argument("--exp_name", default='R2D2', type=str, help="Experiment name.")
+parser.add_argument("--run_name", default='R2D2_10M', type=str, help="Run name.")
+parser.add_argument("--save_dir", default='/home/fassty/Devel/school/diploma_thesis/code/exp_results/',
+                    type=str, help="Where to store the experiments.")
 parser.add_argument("--algo", default='R2D2', type=str, help='Which algorithm to use for the experiment.')
-parser.add_argument("--iterations", default=50, type=int, help="Number of training iterations to run.")
-parser.add_argument("--num_samples", default=1, type=int, help="Number of times to run each experiment.")
+parser.add_argument("--iterations", default=100, type=int, help="Number of training iterations to run.")
+parser.add_argument("--episode_length", default=1000, type=int, help="Length of the episode.")
+parser.add_argument("--num_workers", default=5, type=int, help="Number of parallel workers.")
+parser.add_argument("--num_envs", default=20, type=int, help="Number of vector environments per worker.")
+parser.add_argument("--num_samples", default=5, type=int, help="Number of times to run each experiment.")
+# Options for running benchmark experiments
 parser.add_argument("--n_days", default=None, type=int, help="Number of days to use for training.")
-parser.add_argument('--seed', default=52, type=int, help="Random seed to be used for sampling the days.")
+parser.add_argument('--seed', default=None, type=int, help="Random seed to be used for sampling the days.")
 
 
 def main(args: argparse.Namespace):
     config = (
         R2D2Config()
-        .rollouts(num_rollout_workers=4, num_envs_per_worker=25)
+        .rollouts(num_rollout_workers=args.num_workers, num_envs_per_worker=args.num_envs)
         .resources(num_gpus=1, num_cpus_per_worker=1)
-        .training(model={'use_lstm': True, 'max_seq_len': 120, 'lstm_use_prev_action': True, 'lstm_use_prev_reward': True},
+        .training(model={'use_lstm': True, 'max_seq_len': 80, 'lstm_use_prev_action': True, 'lstm_use_prev_reward': True},
                   replay_buffer_config={'replay_burn_in': 40},
                   zero_init_states=True)
         .environment(env='StockExchangeEnv-v0',
                      env_config={
-                         'sim_config': {'max_steps': 10000},
+                         'sim_config': {'max_steps': args.episode_length},
                          'state_config': {
                              'market_state': ['vwap'],
                              'technical_indicators': [
@@ -56,8 +63,8 @@ def main(args: argparse.Namespace):
                              ]},
                          'exchange_config': {'maker_fee': 1e-3},
                      })
-        .exploration(exploration_config={'epsilon_timesteps': 10_000_000})
-        .reporting(min_sample_timesteps_per_iteration=4 * 25 * 10000, metrics_num_episodes_for_smoothing=4 * 25 * 10000)
+        .reporting(min_sample_timesteps_per_iteration=args.num_workers * args.num_envs * args.episode_length,
+                   metrics_num_episodes_for_smoothing=args.num_workers * args.num_envs * args.episode_length)
         .evaluation(evaluation_interval=1, evaluation_duration=10, evaluation_config={'explore': False})
     )
 
